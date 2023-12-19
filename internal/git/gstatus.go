45fc82c8917143227a83b785fa26a5369ae03bda
Andreas Olsson <olsson.andreas@gmail.com>
2023-12-19 20:20:13 +0100

    first try

--- internal/git/gstatus.go
+++ internal/git/gstatus.go
@@ -1,3 +1,39 @@
 package git
 
+import (
+	"strings"
+)
 
+type GitStatus struct {
+    Staged []string
+    Unstaged []string
+    Untracked []string
+}
+
+func parseGitStatus(status_output string) (*GitStatus, error) {
+    lines := strings.Split(status_output, "\n")
+
+    gs := GitStatus {
+        Staged: []string{},
+        Unstaged: []string{},
+        Untracked: []string{},
+    }
+    for _, v := range lines {
+        lineSplit := strings.Split(strings.Trim(v, " "), " ")
+        t := lineSplit[0]
+        fp := lineSplit[1]
+
+        switch t {
+        case "A":
+            gs.Staged = append(gs.Staged, fp)
+        case "M":
+            gs.Unstaged = append(gs.Unstaged, fp)
+        case "MM":
+            gs.Unstaged = append(gs.Unstaged, fp)
+        case "??":
+            gs.Untracked = append(gs.Untracked, fp)
+        }
+    }
+
+    return &gs, nil
+}
--- internal/git/gstatus_test.go
+++ internal/git/gstatus_test.go
@@ -3,4 +3,32 @@ package git
 import "testing"
 
 func Test(t *testing.T) {
+    status_output := `A README.md
+                      MM internal/git/gstatus.go
+                      M internal/git/gstatus_test.go
+                      ?? cmd/
+                      ?? go.mod`
+
+   gs, err := parseGitStatus(status_output)
+
+   if err != nil {
+       t.Fatalf("Expected err to be null, got %s\n", err)
+   }
+
+   if gs == nil {
+       t.Fatalf("Expected gs to not be null\n")
+   }
+
+   if len(gs.Staged) != 1 {
+       t.Fatalf("Expected to get %d staged files, got %d\n", 1, len(gs.Staged))
+   }
+
+   if len(gs.Unstaged) != 2 {
+       t.Fatalf("Expected to get %d unstaged files, got %d\n", 2, len(gs.Unstaged))
+   }
+
+   if len(gs.Untracked) != 2 {
+       t.Fatalf("Expected to get %d untracked files, got %d\n", 2, len(gs.Untracked))
+   }
 }
+
